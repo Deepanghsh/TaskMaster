030c0ed5eee5ff4536406950dc80e06dc62094d4
Deepanghsh Naik <deepangshnaik1@gmail.com>
2025-12-23 12:58:46 +0530

    feat: add mobile, dob, and gender fields to User schema

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -1,10 +1,36 @@
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  date: { type: Date, default: Date.now }
+  name: { 
+    type: String, 
+    required: true 
+  },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  password: { 
+    type: String, 
+    required: true 
+  },
+  mobile: {
+    type: String,
+    default: ''
+  },
+  dob: {
+    type: String,
+    default: ''
+  },
+  gender: {
+    type: String,
+    enum: ['', 'male', 'female', 'other', 'prefer-not-to-say'],
+    default: ''
+  },
+  date: { 
+    type: Date, 
+    default: Date.now 
+  }
 });
 
 export default mongoose.model('user', UserSchema);
