e428586302bf50623bb3daf67a32b4757186ae59
Deepanghsh Naik <deepangshnaik1@gmail.com>
2025-12-25 17:29:36 +0530

    fix: clean up auth flow and improve state

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -7,11 +7,11 @@ const api = axios.create({
   }
 });
 
-// Add token to requests automatically
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = sessionStorage.getItem('auth_token');
     if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
       config.headers['x-auth-token'] = token;
     }
     return config;
